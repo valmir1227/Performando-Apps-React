0348007d6539eea8a8d6c59b77047641f7bee3eb
Valmir Almeida <almeidavalmir76@gmail.com>
2022-10-10 17:55:18 -0300

    first commit

---
+++ components/SearchResults.tsx
@@ -0,0 +1,19 @@
+import ProductItem from "./ProductItem";
+
+interface SearchResultsProps {
+  results: Array<{
+    id: number;
+    price: number;
+    title: string;
+  }>;
+}
+
+export default function SearchResults({ results }: SearchResultsProps) {
+  return (
+    <div>
+      {results.map((product) => {
+        return <ProductItem product={product} />;
+      })}
+    </div>
+  );
+}
