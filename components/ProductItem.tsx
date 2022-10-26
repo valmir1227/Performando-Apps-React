12f53675aa1182e6a2b1ddaff0a20f2154d408d5
Valmir Almeida <almeidavalmir76@gmail.com>
2022-10-26 11:14:58 -0300

    ...

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,14 +1,18 @@
-import { memo, useState } from 'react';
-import { AddProductToWishlistProps } from './AddProductToWishlist'
-import dynamic from 'next/dynamic';
-import lodash from 'lodash';
+import { memo, useState } from "react";
+import { AddProductToWishlistProps } from "./AddProductToWishlist";
+import dynamic from "next/dynamic";
+import lodash from "lodash";
 
-
-const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
-  return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
-}, {
-  loading: () => <span>Carregando...</span>
-})
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  async () => {
+    return await import("./AddProductToWishlist").then(
+      (mod) => mod.AddProductToWishlist
+    );
+  },
+  {
+    loading: () => <span>Carregando...</span>,
+  }
+);
 
 interface ProductItemProps {
   product: {
@@ -16,7 +20,7 @@ interface ProductItemProps {
     price: number;
     priceFormatted: string;
     title: string;
-  }
+  };
   onAddToWishlist: (id: number) => void;
 }
 
@@ -26,18 +30,22 @@ function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.priceFormatted}</strong>
-      <button onClick={() => setIsAddingToWishlist(true)}>Adicionar aos favoritos</button>
-      
-      { isAddingToWishlist && (
+      <button onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishlist && (
         <AddProductToWishlist
           onAddToWishlist={() => onAddToWishlist(product.id)}
           onRequestClose={() => setIsAddingToWishlist(false)}
         />
-      ) }
+      )}
     </div>
-  )
+  );
 }
 
-export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
-  return lodash.isEqual(prevProps.product, nextProps.product)
-})
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return lodash.isEqual(prevProps.product, nextProps.product);
+  }
+);
